722cf6b32c4e894d6ce15eae73d8685d08a84633
Gajraj <gajrajsingh.chouhan@philips.com>
2020-07-03 14:11:19 +0530

    Removed unused imports.

--- csw/src/main/java/com/philips/platform/csw/dialogs/ProgressDialogFragment.java
+++ csw/src/main/java/com/philips/platform/csw/dialogs/ProgressDialogFragment.java
@@ -3,18 +3,9 @@ package com.philips.platform.csw.dialogs;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.KeyEvent;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 import com.philips.platform.uid.view.widget.AlertDialogFragment;
 
--- csw/src/main/java/com/philips/platform/csw/permission/PermissionFragment.java
+++ csw/src/main/java/com/philips/platform/csw/permission/PermissionFragment.java
@@ -8,8 +8,6 @@
 package com.philips.platform.csw.permission;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
