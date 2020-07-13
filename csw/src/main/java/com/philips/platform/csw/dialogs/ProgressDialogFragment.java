97c8c341d3aa40db6aa5f2ad935b80c20fef6c7f
Gajraj <gajrajsingh.chouhan@philips.com>
2020-07-13 18:11:27 +0530

    Code Review Changes.

--- csw/src/main/java/com/philips/platform/csw/dialogs/ProgressDialogFragment.java
+++ csw/src/main/java/com/philips/platform/csw/dialogs/ProgressDialogFragment.java
@@ -3,10 +3,15 @@ package com.philips.platform.csw.dialogs;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.KeyEvent;
-import androidx.annotation.NonNull;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.philips.platform.csw.permission.PermissionFragment;
 import com.philips.platform.uid.view.widget.AlertDialogFragment;
 
 public class ProgressDialogFragment extends AlertDialogFragment implements DialogInterface.OnKeyListener {
@@ -22,12 +27,33 @@ public class ProgressDialogFragment extends AlertDialogFragment implements Dialo
     @Override
     public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
-            //On Back Key Pressed, Dismissing the dialog and finishing the activity also.
+            //On Back Key Pressed, Dismissing the dialog.
             dialog.dismiss();
-            if (getActivity() != null) {
-                this.getActivity().onBackPressed();
+
+            FragmentManager fragmentManager = getFragmentManager();
+            if (fragmentManager != null) {
+                Fragment currentFragment = getCurrentFragment(fragmentManager);
+                if (currentFragment instanceof PermissionFragment) {
+                    doCallOnBackPressed();
+                } else {
+                    fragmentManager.popBackStack();
+                }
+            } else {
+                doCallOnBackPressed();
             }
         }
         return true;
+    }
+
+    private void doCallOnBackPressed() {
+        if (getActivity() != null) {
+            this.getActivity().onBackPressed();
+        }
+    }
+
+    private Fragment getCurrentFragment(FragmentManager fragmentManager) {
+        String fragmentTag = fragmentManager.getBackStackEntryAt(fragmentManager.getBackStackEntryCount() - 1).getName();
+        Fragment currentFragment = fragmentManager.findFragmentByTag(fragmentTag);
+        return currentFragment;
     }
 }
